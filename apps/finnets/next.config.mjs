7f69eb0ae0c6391e336c5b8795427a587124e8cb
Connor Linville <clinvil2@gmail.com>
2025-11-03 19:32:11 -0800

    Remformatting and DB docker - Added DB docker using MySQL - Fixed .env issues in docker compose file - Moved DB related files to mock_bank_database, this is to:     + Separate responsibility clearly     + Help logical understanding     + Make docker-compose eaiser to use/write

--- apps/finnets/next.config.mjs
+++ apps/finnets/next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') }); // Adjust path as needed
+
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  env: {
+    // You can also explicitly expose specific variables here
+    SHARED_VARIABLE: process.env.SHARED_VARIABLE,
+  },
 };
 
 export default nextConfig;
--- apps/start-image.sh
+++ apps/start-image.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker compose -f docker-compose.dev.yml up -d --build
+docker compose --env-file ../.env -f docker-compose.dev.yml up --build
