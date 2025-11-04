c0d82928ae2991541d7d74a389ff6b622feffbf0
Connor Linville <clinvil2@gmail.com>
2025-11-03 22:03:09 -0800

    LARGE Bug fix to docker — Is now runnable!

--- apps/finnets/next.config.mjs
+++ apps/finnets/next.config.mjs
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
-
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../../.env') }); // Adjust path as needed
-
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
-  env: {
-    // You can also explicitly expose specific variables here
-    SHARED_VARIABLE: process.env.SHARED_VARIABLE,
-  },
 };
 
 export default nextConfig;
