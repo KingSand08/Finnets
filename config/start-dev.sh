754817938bbbeb3245a152fc22afc50f8137a5f6
Connor Linville <clinvil2@gmail.com>
2025-11-08 21:30:54 -0800

    Fixed detach issue on starting docker commands

--- config/start-dev.sh
+++ config/start-dev.sh
@@ -3,12 +3,11 @@ set -e
 
 if docker network inspect bank_dev_net >/dev/null 2>&1; then
   echo "bank_dev_net already exists"
-  docker compose --env-file ../.env -f ../docker-compose.dev.yml up --build
+  docker compose --env-file ../.env -f ../docker-compose.dev.yml up --build -d
   echo "Created dev demo"
 else
   echo "Creating bank_dev_net..."
   docker network create bank_dev_net >/dev/null
   echo "Created bank_shared_net"
-  docker compose --env-file ../.env -f ../docker-compose.dev.yml up --build
-  echo "Created dev demo"
+  docker compose --env-file ../.env -f ../docker-compose.dev.yml up --build  -d
 fi
--- config/start-prod.sh
+++ config/start-prod.sh
@@ -3,12 +3,12 @@ set -e
 
 if docker network inspect bank_prod_net >/dev/null 2>&1; then
   echo "bank_prod_net already exists"
-  docker compose --env-file ../.env -f ../docker-compose.prod.yml up --build
+  docker compose --env-file ../.env -f ../docker-compose.prod.yml up --build -d
   echo "Created prod demo"
 else
   echo "Created bank_prod_net..."
   docker network create bank_prod_net >/dev/null
   echo "Created bank_shared_net"
-  docker compose --env-file ../.env -f ../docker-compose.prod.yml up --build
+  docker compose --env-file ../.env -f ../docker-compose.prod.yml up --build -d
   echo "Created prod demo"
 fi
